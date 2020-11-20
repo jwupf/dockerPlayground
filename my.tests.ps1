70d7a5b66125d4f9099ae15114f567d56fdf8542
Jörg Wunderlich-Pfeiffer <joerg.wunderlich@gmail.com>
2020-11-20 10:52:31 +0100

    Extend a base image

--- my.tests.ps1
+++ my.tests.ps1
@@ -4,8 +4,16 @@ Context "docker setup" {
         $dockerRepoTag = "0.0.1"
         $dockerImageReference = ("{0}:{1}" -f $dockerRepo, $dockerRepoTag)
 
+        $extDockerRepo = "extended"
+        $extDockerRepoTag = "0.0.1"
+        $extDockerImageReference = ("{0}:{1}" -f $extDockerRepo, $extDockerRepoTag)
+
         if($null -ne (docker images -q $dockerImageReference)){
             docker image rm -f $dockerImageReference
+        }
+
+        if($null -ne (docker images -q $extDockerImageReference)){
+            docker image rm -f $extDockerImageReference
         }
     }
 
@@ -22,9 +30,22 @@ Context "docker setup" {
         .\runHelloWorld.ps1 -ImageReference $dockerImageReference | Should -Be "Hello World! From Script!"
     }
 
+    It "build ext image" {
+        .\buildImage.ps1 -DockerPath ".\extImage" -ImageReference $extDockerImageReference
+        docker images -q $extDockerImageReference | Should -Not -BeNullOrEmpty
+    } 
+
+    It "runs ext hello world" {
+        .\runHelloWorld.ps1 -ImageReference $extDockerImageReference | Should -Be "Hello World! From ext. Script!"
+    }
+
     AfterAll{
         if($null -ne (docker images -q $dockerImageReference)){
             docker image rm -f $dockerImageReference
+        }
+
+        if($null -ne (docker images -q $extDockerImageReference)){
+            docker image rm -f $extDockerImageReference
         }
     }
 }
