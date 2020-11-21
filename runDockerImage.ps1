e56e9e879c2dcd2a81d8cda428cd57a512a2a9aa
Jörg Wunderlich-Pfeiffer <joerg.wunderlich@gmail.com>
2020-11-21 10:43:56 +0100

    Refactor

--- runDockerImage.ps1
+++ runDockerImage.ps1
@@ -6,6 +6,6 @@ param(
     $Mapping #{$srcFolder,"/src"},{$outFolder,"/out"}
 )
 
-$map = $Mapping| Where-Object {$null -ne $_} | ForEach-Object { "-v {0}:{1}" -f $_[0],$_[1] }
+$map = $Mapping| Where-Object {$null -ne $_} | ForEach-Object { "-v {0}" -f ($_ -join ':') }
 $process = "docker","run","--rm","-it" + $map + $ImageReference + $Command
 Invoke-Expression  ($process -join ' ')
